c63eef35a826fdbd17cb94242308f0d4be9a54ba
Ho Wun Mok <hmok11@my.centennialcollege.ca>
2022-10-26 03:59:32 -0400

    updated URI DB

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    "URI": "mongodb://localhost/mean-book"
+    //"URI": "mongodb://localhost/mean-book"
+    "URI": "mongodb+srv://HoWunMok:HoWunMok1234@mongodbserver-demo.gpc2zvz.mongodb.net/book_store?retryWrites=true&w=majority"
 }
